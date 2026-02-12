c6cde203220734d831338c76a113e6a69cc134b3
Frederikke Schmidt <fredeschmidt92@gmail.com>
2026-02-12 08:06:06 +0100

    removed all components

--- assets/components/textbox/textbox.js
+++
@@ -1,6 +0,0 @@
-function initTextboxComponent() {
-  const textboxes = document.querySelectorAll('[data-component="textbox"]');
-  if (!textboxes.length) return;
-}
-
-export { initTextboxComponent };
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,3 @@
 import { initThemeSwitcher } from "./theme.js";
-import { initQuoteComponent } from "./components/quote/quote.js";
-import { initTextboxComponent } from "./components/textbox/textbox.js";
 
 initThemeSwitcher();
-initQuoteComponent();
-initTextboxComponent();
